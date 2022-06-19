6f7fbaf45e3ab56521d5d9d2501856b65e445286
ErvinCA <ervinca96@gmail.com>
2022-06-19 11:17:54 +0700

    Recyclerview APP

---
+++ app/src/main/java/com/ervinchoirulanwar/recyclerview/model/Pemain.kt
@@ -0,0 +1,10 @@
+package com.ervinchoirulanwar.recyclerview.model
+
+data class Pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi : String,
+    var tinggi : String,
+    var tempatlahir : String,
+    var tgllahir : String
+)
